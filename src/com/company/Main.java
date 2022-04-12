2b359f326a12c208666ce7db049ecedf8b2e3222
WaleSalami10 <100866152+WaleSalami10@users.noreply.github.com>
2022-04-12 13:18:48 -0500

    hello world commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+
+        Product product=new Product("keyboard");
+        System.out.println(product.getName());
+    }
+}
